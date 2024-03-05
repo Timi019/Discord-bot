cba0c3ef8e90b11ca4808288e48cbd3721cb92e3
Timi019 <130039879+Timi019@users.noreply.github.com>
2024-03-05 17:49:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,66 @@
 import discord
-from bot_logic import *
-
-# Zmienna intencje przechowuje uprawnienia bota
+from discord.ext import commands
+from discord.ext import tasks
+import os
+import random
+import requests
 intents = discord.Intents.default()
-# Włączanie uprawnienia do czytania wiadomości
 intents.message_content = True
-# Tworzenie bota w zmiennej klienta i przekazanie mu uprawnień
-client = discord.Client(intents=intents)
-token = "MTE5NTczMDQ4MDAwODUzMjA3OQ.GqZ6NV.slDXJ2KOLbl7M-PE3ISG0D9LgTuxLp971KrrHg"
-prefix = '/'
-@client.event
+counter = 0
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
 async def on_ready():
-    print(f'Zalogowano się jako {client.user}')
+    print(f'Zalogowaliśmy się jako {bot.user}')
 
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Cześć, jestem bot{bot.user}!')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:   
-        return
-    if message.content.startswith(prefix+'hello'):
-        await message.channel.send("Cześć!")
-    elif message.content.startswith(prefix+'bye'):
-        await message.channel.send("\U0001f642")
-    elif message.content.startswith(prefix+'generate_password'):
-        await message.channel.send(gen_pass(10) + " Twoje haslo ma 10 znaków")
-    elif message.content.startswith(prefix+'smile'):
-        await message.channel.send(gen_emoji())
-    elif message.content.startswith(prefix+'flip_coin'):
-        await message.channel.send(flip_coin())
-    
-client.run(token)
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def mem(ctx):
+    with open('vip.png', 'rb') as f:
+        # Zapiszmy przekonwertowany plik biblioteki Discord w tej zmiennej!
+        picture = discord.File(f)
+    # Możemy następnie wysłać ten plik jako parametr!
+    await ctx.send(file=picture)
+
+@bot.command()
+async def ranmem(ctx):
+    imgs = os.listdir('images')
+    image = random.choice(imgs)
+    with open(f'images/{image}', 'rb') as f:
+            picture = discord.File(f)
+    await ctx.send(file=picture)
+
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+@bot.command('duck')
+async def duck(ctx):
+    '''Po wywołaniu polecenia duck program wywołuje funkcję get_duck_image_url'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+@bot.event
+async def on_member_join(self, member):
+        guild = member.guild
+        if guild.system_channel is not None:
+            to_send = f'Welcome {member.mention} to {guild.name}!'
+            await guild.system_channel.send(to_send)
+
+@tasks.loop(minutes=1.0, count=None)
+async def my_background_task():
+    global counter
+    channel = bot.get_channel(1234) # channel id as an int
+    counter += 1
+    await channel.send(f'{counter}')
+my_background_task.start()
+bot.run("Your token")
